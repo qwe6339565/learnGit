1f9aac453514396e3cc81c14f51fa2ccb18f7fac
激动的马 <532137371@qq.com>
2017-12-07 10:23:57 +0800

    1.删除几个没用的文件 2.添加一个demo文件

---
+++ gitDemoCode/gitDemoCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitDemoCode
+//
+//  Created by wx on 2017/12/7.
+//  Copyright © 2017年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
--- readme.txt
+++
@@ -1,5 +0,0 @@
-tag study
-hello world
-new branch dev insert
-creating a new branch is quick 
-creating a new branch is quick and simple 
